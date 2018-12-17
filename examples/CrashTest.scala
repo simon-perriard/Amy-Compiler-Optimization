d720ff93fb245a7d0a515748aa938feab57e54e0
Simon Perriard <simon.perriard@epfl.ch>
2018-12-17 14:49:29 +0100

    Layout

--- examples/CrashTest.scala
+++ examples/CrashTest.scala
@@ -1,3 +1,12 @@
 object CrashTest {
 
+  ~This function involves love @see Std.printInt
+  @param marie es la _copiña_
+  @param love ouiiiiiiiiiiiiiiiii
+  @return dat love
+  ~
+
+  def benito(marie: Int, love: Boolean): Boolean = {
+    true
+  }
 }
